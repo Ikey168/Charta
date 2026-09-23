adea8cf9f0263d642a14978ceb2091bfd111230f
KrasForge <dev-github@krasnjanski-mail.com>
2026-09-23 03:09:39 +0200

    Retain release native symbols and declare test runner

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -71,4 +71,5 @@ dependencies {
     implementation("androidx.core:core:1.17.0")
     androidTestImplementation("androidx.test:core:1.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
 }
